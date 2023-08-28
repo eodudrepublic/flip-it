b88852954adfb318af39e685e582b037e0dbe1ba
tkim105 <tkim105@jh.edu>
2023-08-28 11:11:24 +0900

    finished setting up the new branch
    run configuration 이 되지 않아서 다시 프로젝트 생성하고 .dart 파일들 옮겨서 실행 되는 것 확인함.

--- test/TMK/flipit/lib/main.dart
+++ test/TMK/flipit/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'ui4.dart';
-import 'ui3.dart';
-import 'ui1.dart';
 
 void main() {
-  runApp(MaterialApp(home:UI4()));
+  runApp(MaterialApp(home: UI4()));
 }
 
 class MyApp extends StatelessWidget {
