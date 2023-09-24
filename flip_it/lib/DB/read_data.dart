573e6ea6ce7313295611d24243a7df6a8e22eaea
Daeyoung Kim <eodudrepublic@gmail.com>
2023-09-25 01:38:07 +0900

    포스티잇 플립 기능 구현
    플립할때 코인 차감, 누가 나를 플립했는지 저장, 내가 누굴 플립했는지 저장하고 이후 그 포스트잇을 플립할때는 코인 차감 안되는 기능 구현

--- flip_it/lib/DB/read_data.dart
+++ flip_it/lib/DB/read_data.dart
@@ -5,8 +5,8 @@ import 'package:flip_it/test.dart';
 import 'package:flutter/material.dart';
 
 // 현재 문제점 :
-// 1. 플립한 uid, 플립당한 uid가 각각 저장되지 않음 -> 플립할때마다 코인 소모
-// 2. 플립해도 세부 내용 보이지 않음
+// 1. 플립한 uid, 플립당한 uid가 각각 저장되지 않음 -> 플립할때마다 코인 소모 => 해결!
+// 2. 플립해도 세부 내용 보이지 않음 => 해결!
 
 class ReadData extends StatefulWidget {
   const ReadData({super.key});
@@ -18,22 +18,35 @@ class ReadData extends StatefulWidget {
 class _ReadDataState extends State<ReadData> {
   int? currentUserCoin;
   Map<String, dynamic> expandedData = {};
+  String? currentlyExpandedUid;
 
   Future<void> updateMyFlipsAndFlippedMe(String uid) async {
     final user = FirebaseAuth.instance.currentUser;
     if (user == null) return;
 
-    // my_flips 업데이트
-    DocumentReference myDocRef = FirebaseFirestore.instance.collection('season_data').doc(user.uid);
-    await myDocRef.update({
-      'My_flips': FieldValue.arrayUnion([uid])  // Only add uid for simplicity
-    });
+    // Check if user already flipped the uid before
+    DocumentSnapshot myDataSnapshot = await FirebaseFirestore.instance.collection('season_data').doc(user.uid).get();
+    List<dynamic> myFlips = myDataSnapshot['My_flips'] ?? [];
 
-    // flipped_me 업데이트
-    DocumentReference theirDocRef = FirebaseFirestore.instance.collection('season_data').doc(uid);
-    await theirDocRef.update({
-      'Flipped_me': FieldValue.arrayUnion([user.uid])
-    });
+    if (!myFlips.contains(uid)) {
+      // my_flips 업데이트
+      DocumentReference myDocRef = FirebaseFirestore.instance.collection('season_data').doc(user.uid);
+      await myDocRef.update({
+        'My_flips': FieldValue.arrayUnion([uid])
+      });
+
+      // flipped_me 업데이트
+      DocumentReference theirDocRef = FirebaseFirestore.instance.collection('season_data').doc(uid);
+      await theirDocRef.update({
+        'Flipped_me': FieldValue.arrayUnion([user.uid])
+      });
+
+      // 코인 업데이트
+      if (currentUserCoin! >= 500) {
+        await FirebaseFirestore.instance.collection('user_data').doc(user.uid).update({'coin': currentUserCoin! - 500});
+        currentUserCoin = currentUserCoin! - 500;
+      }
+    }
   }
 
   @override
@@ -88,35 +101,25 @@ class _ReadDataState extends State<ReadData> {
               itemBuilder: (context, index) {
                 return Card(
                   child: ExpansionTile(
+                    initiallyExpanded: currentlyExpandedUid == docs[index].id,  // Keep the tile expanded based on the UID
                     title: Text("UID: ${docs[index].id}"),
                     onExpansionChanged: (expanded) async {
                       if (expanded) {
+                        currentlyExpandedUid = docs[index].id;  // Save the UID of the currently expanded tile
                         expandedData = docs[index].data() as Map<String, dynamic>;
 
                         DocumentSnapshot userData = await FirebaseFirestore.instance.collection('user_data').doc(currentUserUid).get();
                         currentUserCoin = userData['coin'];
 
-                        if (currentUserCoin! >= 500) {
-                          await FirebaseFirestore.instance.collection('user_data').doc(currentUserUid).update({'coin': currentUserCoin! - 500});
-
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text('500코인을 소모해 내용을 표시했습니다! 남은 코인 : ${currentUserCoin! - 500}'),
-                          ));
-
-                          // Firestore 업데이트
-                          await updateMyFlipsAndFlippedMe(docs[index].id);
-                          setState(() {});  // Force a rebuild to update the displayed data
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text('coin이 충분하지 않습니다! 현재 코인 : $currentUserCoin'),
-                          ));
-                        }
+                        // Firestore 업데이트
+                        await updateMyFlipsAndFlippedMe(docs[index].id);
+                        setState(() {});  // Force a rebuild to update the displayed data
                       }
                     },
                     children: [
                       Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: (currentUserCoin != null && currentUserCoin! >= 500) ? Text(jsonEncode(expandedData)) : Text('코인이 부족하여 내용을 불러올 수 없습니다.'),
+                        child: Text(jsonEncode(expandedData)),
                       ),
                     ],
                   ),
