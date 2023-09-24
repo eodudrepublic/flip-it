1987323ed44cc5ff2637b309355d2eb539faa5e0
tkim105 <tkim105@jh.edu>
2023-09-25 08:51:30 +0900

    ui8 수정

--- test/TMK/flipit/lib/ui8.dart
+++ test/TMK/flipit/lib/ui8.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:async';
+import 'ui9.dart';
 
-class Verif2 extends StatelessWidget {
+class UI8 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: UI8(),
+      home: SignUp(),
     );
   }
 }
 
-class UI8 extends StatefulWidget {
-  const UI8({super.key});
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
 
   @override
-  State<UI8> createState() => _UI8State();
+  State<SignUp> createState() => _SignUpState();
 }
 
-class _UI8State extends State<UI8> {
+class _SignUpState extends State<SignUp> {
   final TextEditingController _emailController = TextEditingController();
-  List<TextEditingController> _controllers =
-  List.generate(6, (_) => TextEditingController());
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _checkController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  List<TextEditingController> _controllers = List.generate(6, (_) => TextEditingController());
   bool _isVerificationVisible = false;
+  bool _isVerificationButtonPressed = false;
   int _secondsRemaining = 180;
   Timer? _timer;
 
   void _toggleVerificationVisibility() {
+    if (_isVerificationButtonPressed) {
+      return;  // 본인인증 버튼은 단 한 번만 누를 수 있도록 설정
+    }
     if (_isVerificationVisible) {
       _cancelTimer();
     }
@@ -36,6 +43,16 @@ class _UI8State extends State<UI8> {
         startTimer();
       }
     });
+    _isVerificationButtonPressed = true;
+  }
+
+  void _handleConfirmation() {
+    if (_isVerificationVisible) {
+      _toggleVerificationVisibility();
+    }
+    // 여기에 확인 버튼을 눌렀을 때 해야할 동작 추가
+    // 일치하면 인증번호창 접히고, 본인인증 버튼이 Text('인증 성공 ✓', style: TextStyle(color: Color(0xFF4B2FFE)) 으로 바뀌고
+    // 불일치하면 일치하지 않으니 재입력하라는 메시지가 인증번호창 내에 뜨도록
   }
 
   @override
@@ -75,6 +92,19 @@ class _UI8State extends State<UI8> {
         });
       }
     });
+  }
+  bool isPasswordValid(String password) {
+    // 대문자 포함, 최소 8글자, 최대 16글자
+    final RegExp regex = RegExp(r'^(?=.*[A-Z])(.{8,16})$');
+    return regex.hasMatch(password);
+  }
+
+  bool get isConfirmationButtonEnabled {
+    return _emailController.text.isNotEmpty && //본인인증이 인증완료 되었을 때로 변경해야함
+        isPasswordValid(_passwordController.text) &&
+        _checkController.text.isNotEmpty &&
+        _passwordController.text == _checkController.text &&
+        _nameController.text.isNotEmpty;
   }
 
   @override
@@ -126,16 +156,24 @@ class _UI8State extends State<UI8> {
                           controller: _emailController,
                           decoration: InputDecoration(
                             hintText: '부산대 이메일 아이디',
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFF4B2FFE)),
+                            ),
                           ),
                         ),
                       ),
                       Text('@pusan.co.kr'),
                       SizedBox(width: 15.0),
                       ElevatedButton(
-                        onPressed: _toggleVerificationVisibility,
-                        child: Text('본인인증'),
+                        onPressed: (){
+                          if(_emailController.text.isNotEmpty){
+                            _toggleVerificationVisibility();
+                          }
+                          else null;
+                        },
+                        child: Text('본인 인증'),
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFF4B2FFE),
+                          backgroundColor: _emailController.text.isNotEmpty ? Color(0xFF4B2FFE) : Color(0xFFE5E5E5),
                           foregroundColor: Colors.white,
                           elevation: 0,
                           padding: EdgeInsets.symmetric(
@@ -213,7 +251,9 @@ class _UI8State extends State<UI8> {
                               ElevatedButton(
                                 onPressed: (){
                                   _cancelTimer();
-                                  _toggleVerificationVisibility(); //데이터 전송 + 인증 완료 후 이게 작동되어야 함
+                                  setState((){
+                                    _isVerificationVisible = false;
+                                  }); //데이터 전송 + 인증 완료 후 이게 작동되어야 함
                                 },
                                 child: Text('확인'),
                                 style: ElevatedButton.styleFrom(
@@ -232,7 +272,125 @@ class _UI8State extends State<UI8> {
                           ),
                         ),
                       ),
-                    )
+                    ),
+                  SizedBox(height: screenHeight*0.025),
+                  Text(
+                    '비밀번호',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Row(
+                    children:[
+                      Expanded(
+                        child: TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: '비밀번호 (대문자 포함 8~16글자)',
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFF4B2FFE)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight*0.025),
+                  Text(
+                    '비밀번호 확인',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Row(
+                    children:[
+                      Expanded(
+                        child: TextField(
+                          controller: _checkController,
+                          obscureText: true,
+                          onChanged: (value) {
+                            setState(() {
+                              // Add code to update the UI based on password confirmation
+                            });
+                          },
+                          decoration: InputDecoration(
+                            hintText: '비밀번호 재입력',
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFF4B2FFE)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      isPasswordValid(_passwordController.text) &&
+                          _checkController.text.isNotEmpty &&
+                          _passwordController.text == _checkController.text
+                          ? Text('일치 ✓', style: TextStyle(color: Color(0xFF4B2FFE)))
+                          : (_checkController.text.isNotEmpty
+                          ? Text('불일치 X', style: TextStyle(color: Colors.red))
+                          : Text('')),
+
+                    ],
+                  ),
+                  SizedBox(height: screenHeight*0.025),
+                  Text(
+                    '이름',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Row(
+                    children:[
+                      Expanded(
+                        child: TextField(
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            hintText: '다른 사람들에게는 공개되지 않아!',
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFF4B2FFE)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight*0.025),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: (){
+                          if (isConfirmationButtonEnabled) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => UI9()),
+                            );
+                          }
+                        },  //다음 페이지로
+                        child: Text('확인'),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(screenWidth*0.8, 0),
+                          backgroundColor: isConfirmationButtonEnabled //왜 안될까...
+                              ? Color(0xFF4B2FFE)  // 활성화되었을 때 파란색
+                              : Color(0xFFE5E5E5),
+                          foregroundColor: Colors.white,
+                          elevation: 0,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: screenWidth * 0.03,
+                              vertical: screenHeight * 0.01),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -267,7 +425,7 @@ class _UI8State extends State<UI8> {
                     borderSide: BorderSide(color: Colors.black),
                   ),
                   focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
+                    borderSide: BorderSide(color: Color(0xFF4B2FFE)),
                   ),
                 ),
               ),
